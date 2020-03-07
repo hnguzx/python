ba422dff49d54c38ee4743bd2f4855b1b4b1930c
hnguzx <2544697696@qq.com>
2020-03-07 11:10:15 +0800

    1

--- Demo.py
+++ Demo.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
+
 # 这是一行注释
 # createTime 2020/01/06
-print ("hello")
+print("hello")
 
-print ("guzx") #可以在一行后面添加注释
+print("guzx")  # 可以在一行后面添加注释
 
-print ("进行计算:")
+print("进行计算:")
 
-print ("1+2 = ")
-print (1 + 2)
+print("1+2 = ")
+print(1 + 2)
 
-print (1<=9)
+print(1 <= 9)
 
-print (5/3)
+print(5 / 3)
 
-print (5/2)
+print(5 / 2)
 
 a = 100
 b = 9
 user_name = "guzx"
 password = "123"
 
-print (user_name)
-print (a * b)
+print(user_name)
+print(a * b)
 
-print (user_name,a*b,"这是中间的文字",password)
+print(user_name + "z找那个万 ", a + b)
 # 这是最后的注释！
